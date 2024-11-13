9b52c9cbe3cca122315a37c58034c4c984408be3
ppmarkek <153396079+ppmarkek@users.noreply.github.com>
2024-11-13 17:37:52 +0200

    feat: add shaders

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
 
 const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -22,6 +24,19 @@ controls.dampingFactor = 0.05;
 controls.screenSpacePanning = false;
 controls.minDistance = 2;
 controls.maxDistance = 10;
+
+//Post Process
+const renderPass = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  1.5,
+  0.5,
+  0.2
+);
+
+const composer = new EffectComposer(renderer);
+composer.addPass(renderPass);
+composer.addPass(bloomPass);
 
 const ambientLight = new THREE.AmbientLight("#fff", 10);
 
@@ -59,10 +74,36 @@ loader.load(
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
+//Shader
+const vertexShader = `
+    varying vec3 vPosition;
+
+    void main() {
+        vPosition = position;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+`;
+
+const fragmentShader = `
+    varying vec3 vPosition;
+
+    void main() {
+        gl_FragColor = vec4(abs(vPosition.x), abs(vPosition.y), abs(vPosition.z), 1.0);
+    }
+`;
+
+const shaderMaterial = new THREE.ShaderMaterial({
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+});
+
+const newCube = new THREE.Mesh(new THREE.BoxGeometry(), shaderMaterial);
+
 // scene.add(cube);
 // scene.add(sphere);
 // scene.add(dirLight);
 scene.add(ambientLight);
+scene.add(newCube);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -109,7 +150,7 @@ const animate = () => {
       y: 1.5,
       z: 1.5,
       duration: 1.5,
-      ease: " power1.out",
+      ease: "power1.out",
     });
   } else if (intersects.length === 0 && isHovered) {
     cube.material = originMaterial;
@@ -120,13 +161,13 @@ const animate = () => {
       y: 1,
       z: 1,
       duration: 1.5,
-      ease: " power1.out",
+      ease: "power1.out",
     });
   }
 
   controls.update();
-  renderer.setClearColor("#fff");
-  renderer.render(scene, camera);
+  renderer.setClearColor("blue");
+  composer.render();
 };
 
 animate();
